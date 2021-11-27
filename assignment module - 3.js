cb4f4ede330b8c44a29271b81d44250490654609
Vidyasagar5566 <85301020+Vidyasagar5566@users.noreply.github.com>
2021-11-27 15:49:33 +0530

    Delete assignment module - 3.js

--- assignment module - 3.js
+++
@@ -1,9 +0,0 @@
-var name = ["Ramana","vidya","joe","sagar","Jhon","jogesh","srinivas Chandu","Jagadesh"];
-for (var pop in name) {
-	if (pop[0] == "j" || pop[0] == "J") {
-		console.log("Goodbye " + pop);
-	}
-	else {
-		console.log("Hello " + pop);
-	}
-}
